7afb4b533b1636911523f2ef30f0739f1a62149a
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2022-02-11 17:51:18 -0500

    GameScreen Dev

--- src/screens/GameScreen/GameScreen.js
+++ src/screens/GameScreen/GameScreen.js
@@ -1,7 +1,18 @@
 import { Text, Button } from "react-native";
-import { GameScreenStyled, IntroScreenNavButton, HeaderView } from "./styles";
+import { Dimensions } from "react-native";
+import {
+  GameScreenStyled,
+  IntroScreenNavButton,
+  HeaderView,
+  WordleLetter,
+  WordleLetterView,
+  WordleGameView,
+} from "./styles";
 
 export default function GameScreen({ navigation }) {
+  const screenHeight = Dimensions.get("window").height;
+  const screenWidth = Dimensions.get("window").width - 20 + "px";
+
   return (
     <GameScreenStyled>
       <HeaderView>
@@ -12,6 +23,50 @@ export default function GameScreen({ navigation }) {
           onPress={() => navigation.navigate("IntroScreen")}
         />
       </HeaderView>
+      <WordleGameView width={screenWidth}>
+        <WordleLetterView>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+        </WordleLetterView>
+        <WordleLetterView>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+        </WordleLetterView>
+        <WordleLetterView>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+        </WordleLetterView>
+        <WordleLetterView>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+        </WordleLetterView>
+        <WordleLetterView>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+        </WordleLetterView>
+        <WordleLetterView>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+          <WordleLetter>J</WordleLetter>
+        </WordleLetterView>
+      </WordleGameView>
     </GameScreenStyled>
   );
 }
--- src/screens/GameScreen/styles.js
+++ src/screens/GameScreen/styles.js
@@ -5,6 +5,7 @@ export const GameScreenStyled = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 export const IntroScreenNavButton = styled(Icon)``;
@@ -14,4 +15,25 @@ export const HeaderView = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+`;
+
+export const WordleGameView = styled.View`
+  width: ${(props) => props.width};
+`;
+
+export const WordleLetterView = styled.View`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const WordleLetter = styled.Text`
+  font-size: ${(props) => props.theme.fontSize.xxxlarge};
+  color: ${(props) => props.theme.fontColor};
+  font-weight: bold;
+  flex: 1;
+  text-align: center;
+  margin: 10px;
+  border-width: 2px;
+  border-color: ${(props) => (props.color ? props.color : "grey")};
+  background-color: ${(props) => (props.color ? props.color : "transparent")};
 `;
